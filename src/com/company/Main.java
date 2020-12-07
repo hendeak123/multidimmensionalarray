aabfacfe5990a417f4ec8772d19a49db7369c080
hendeak123 <alyssa.henderson@svu.edu>
2020-12-07 16:01:58 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.lang.reflect.Array;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int multiArray[][] = { {1, 2, 3, 4, 5}, {6, 7, 8, 9, 10} };
+        printArray(multiArray);
+    }
+
+    private static void printArray(int[][] multiArray) {
+        for (int countOne = 0; countOne < multiArray.length; countOne++) {
+            for (int countTwo = 0; countTwo < multiArray[countOne].length; countTwo++) {
+                System.out.println(multiArray[countOne][countTwo]);
+            }
+        }
+    }
+}
